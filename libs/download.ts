422c74757afcd789797690089b88de3176abd391
GGG KILLER <gggkiller2@gmail.com>
2018-11-27 20:32:22 -0200

    Replace recursion with flattening and a queue

--- libs/download.ts
+++ libs/download.ts
@@ -6,7 +6,7 @@ const requestProgress = require("request-progress");
 
 import { ITable } from "./markdown";
 import { ModManifest } from "./mod-manifest";
-import { CurseforgeMod } from "./curseforge";
+import { Mod, getCurseforgeFileID } from "./curseforge";
 import { ModTable } from "./mod-table";
 import { packModId, sanitizeFileName } from "./utils";
 
@@ -21,6 +21,12 @@ export interface IDownloadProgress {
 		elapsed: number,    // The total elapsed seconds since the start (3 decimals)
 		remaining: number   // The remaining seconds to finish (3 decimals)
 	}
+}
+
+interface IDownloadQueueItem {
+	id: string,
+	url: string,
+	version: string
 }
 
 export async function download(url: string, progressCallback: ((_: IDownloadProgress) => void)|null = null): Promise<[Buffer, string]>
@@ -54,81 +60,6 @@ export async function download(url: string, progressCallback: ((_: IDownloadProg
 function hash(hash: string, data: Buffer): string
 {
 	return crypto.createHash(hash).update(data).digest("hex");
-}
-
-function hashFile(hash: string, path: fs.PathLike): Promise<string>
-{
-	return new Promise((resolve, fail) =>
-	{
-		const hashInstance = crypto.createHash(hash);
-		const stream = fs.createReadStream(path);
-		stream.on('end', _ =>
-		{
-			stream.close();
-			resolve(hashInstance.digest('hex'));
-		});
-		stream.pipe(hashInstance);
-	});
-}
-
-async function downloadCurseforgeMod(enabledMods: { [_: string]: true }, manifestPath: string, manifest: ModManifest, minecraftVersion: string, namespace: string, mod: CurseforgeMod, modDirectory: string, progress: string, log: (_: string) => void)
-{
-	// Download new version
-	const version = mod.urls[minecraftVersion]!.fileId;
-	const downloadUrl = mod.urls[minecraftVersion]!.downloadURL;
-	const isDependency = progress.lastIndexOf('[') !== 0;
-
-	if(isDependency)
-		enabledMods[packModId(namespace, mod.id)] = true;
-
-	const existingFileName = manifest.getModFileName(namespace, mod.id);
-	if (mod.hasBeenDownloaded || manifest.getModVersion(namespace, mod.id) != version ||
-		(existingFileName != null && (!fs.existsSync(modDirectory + "/" + existingFileName) ||
-			await hashFile("sha256", `${modDirectory}/${existingFileName}`) != manifest.getModFileSHA256(namespace, mod.id))))
-	{
-		// Remove existing jar
-		if (existingFileName != null)
-		{
-			try
-			{
-				fs.unlinkSync(modDirectory + "/" + existingFileName);
-				log(progress + " - " + packModId(namespace, mod.id) + " " + existingFileName);
-			}
-			catch (e)
-			{
-				if (e.code != "ENOENT") { throw e; }
-			}
-		}
-
-		// Download new jar
-		let [data, fileName] = await download(mod.urls[minecraftVersion]!.downloadURL);
-		fileName = sanitizeFileName(fileName);
-		fs.writeFileSync(modDirectory + "/" + fileName, data);
-		log(progress + " + " + packModId(namespace, mod.id) + " " + fileName);
-
-		manifest.updateMod(namespace, mod.id, fileName, downloadUrl, version, hash("md5", data), hash("sha256", data));
-		manifest.save(manifestPath);
-	}
-	else
-	{
-		log(progress + "   " + packModId(namespace, mod.id) + " " + existingFileName);
-	}
-
-	// Check for dependencies (just in case™)
-	const dependencies = mod.dependencies[minecraftVersion];
-	if(dependencies && dependencies.length > 0)
-	{
-		log(progress + "   " + packModId(namespace, mod.id) + ": Downloading dependencies.");
-		const dependencyCount = dependencies.length;
-		for(let i = 0; i < dependencyCount; i++)
-		{
-			let dependencyProgress = (i + 1).toString();
-			dependencyProgress = " ".repeat(Math.max(0, dependencyCount.toString().length - dependencyProgress.length)) + dependencyProgress;
-			dependencyProgress = `${progress}[${dependencyProgress}/${dependencyCount}]`;
-
-			await downloadCurseforgeMod(enabledMods, manifestPath, manifest, minecraftVersion, namespace, dependencies[i], modDirectory, dependencyProgress, log);
-		}
-	}
 }
 
 export async function downloadMods(modTables: ITable[], minecraftVersion: string, modDirectory: string, manifestPath: string, log: (_: string) => void): Promise<void>
@@ -165,78 +96,89 @@ export async function downloadMods(modTables: ITable[], minecraftVersion: string
 
 			enabledMods[packModId(namespace, id)] = true;
 
+			const url = modTable.getModUrl(i, minecraftVersion);
+			if (url == null)
+			{
+				log(progress + " ! " + packModId(namespace, id) + ": No download URL for Minecraft " + minecraftVersion + " found!");
+				continue;
+			}
+
+			let downloadQueue: IDownloadQueueItem[] = [];
 			switch (namespace)
 			{
 				case "curseforge":
-				{
-					const mod = (await CurseforgeMod.fromID(id, [minecraftVersion]))!;
-					const url = mod.urls[minecraftVersion];
-					if(!url)
-					{
-						log(`${progress} ! ${packModId(namespace, id)}: No download URL for Minecraft ${minecraftVersion} found!`);
-						continue;
-					}
-
-					await downloadCurseforgeMod(enabledMods, manifestPath, manifest, minecraftVersion, namespace, mod, modDirectory, progress, log);
-					break;
-				}
-				case "url":
-				{
-					// Get download URL
-					const url = modTable.getModUrl(i, minecraftVersion);
-					if (url == null)
+					const mod = (await Mod.fromID(id, [minecraftVersion]))!;
+					if(!mod.availableForVersion(minecraftVersion))
 					{
 						log(progress + " ! " + packModId(namespace, id) + ": No download URL for Minecraft " + minecraftVersion + " found!");
-						continue;
-					}
-					let downloadUrl = url;
-					let version = url;
-
-					// Bail if no download URL found
-					if (downloadUrl == null || version == null)
-					{
-						log(progress + " ! " + packModId(namespace, id) + ": Unable to download!");
-						continue;
+						break;
 					}
 
-					// Download new version
-					const existingFileName = manifest.getModFileName(namespace, id);
-					if (manifest.getModVersion(namespace, id) != version ||
-						(existingFileName != null && (!fs.existsSync(`${modDirectory}/${existingFileName}`) ||
-							await hashFile("sha256", `${modDirectory}/${existingFileName}`) != manifest.getModFileSHA256(namespace, id))))
+					downloadQueue.push({
+						id: mod.id,
+						url: mod.urls[minecraftVersion],
+						version: getCurseforgeFileID(mod.urls[minecraftVersion])!
+					});
+
+					for(let dependency of await mod.getDependencies(minecraftVersion))
 					{
-						// Remove existing jar
-						if (existingFileName != null)
+						if(dependency.availableForVersion(minecraftVersion))
 						{
-							try
-							{
-								fs.unlinkSync(modDirectory + "/" + existingFileName);
-								log(progress + " - " + packModId(namespace, id) + " " + existingFileName);
-							}
-							catch (e)
-							{
-								if (e.code != "ENOENT") { throw e; }
-							}
+							downloadQueue.push({
+								id: dependency.id,
+								url: dependency.urls[minecraftVersion],
+								version: getCurseforgeFileID(dependency.urls[minecraftVersion])!
+							});
 						}
-
-						// Download new jar
-						let [data, fileName] = await download(downloadUrl);
-						fileName = sanitizeFileName(fileName);
-						fs.writeFileSync(modDirectory + "/" + fileName, data);
-						log(progress + " + " + packModId(namespace, id) + " " + fileName);
-
-						manifest.updateMod(namespace, id, fileName, downloadUrl, version, hash("md5", data), hash("sha256", data));
-						manifest.save(manifestPath);
-					}
-					else
-					{
-						log(progress + "   " + packModId(namespace, id) + " " + existingFileName);
 					}
 					break;
-				}
+				case "url":
+					// Get download URL
+					downloadQueue.push({
+						id,
+						url,
+						version: url
+					});
+					break;
 				default:
 					log(progress + " ! " + packModId(namespace, id) + ": Cannot derive download URL!");
 					break;
+			}
+
+			// Download new version
+			for(let {id: itemId, url: itemUrl, version: itemVersion} of downloadQueue)
+			{
+				const existingFileName = manifest.getModFileName(namespace, itemId);
+				if (manifest.getModVersion(namespace, itemId) != itemVersion ||
+					(existingFileName != null && !fs.existsSync(`${modDirectory}/${existingFileName}`)))
+				{
+					// Remove existing jar
+					if (existingFileName != null)
+					{
+						try
+						{
+							fs.unlinkSync(modDirectory + "/" + existingFileName);
+							log(progress + " - " + packModId(namespace, id) + " " + existingFileName);
+						}
+						catch (e)
+						{
+							if (e.code != "ENOENT") { throw e; }
+						}
+					}
+
+					// Download new jar
+					let [data, fileName] = await download(itemUrl);
+					fileName = sanitizeFileName(fileName);
+					fs.writeFileSync(modDirectory + "/" + fileName, data);
+					log(progress + " + " + packModId(namespace, id) + " " + fileName);
+
+					manifest.updateMod(namespace, itemId, fileName, itemUrl, itemVersion, hash("md5", data), hash("sha256", data));
+					manifest.save(manifestPath);
+				}
+				else
+				{
+					log(progress + "   " + packModId(namespace, id) + " " + existingFileName);
+				}
 			}
 		}
 	}
